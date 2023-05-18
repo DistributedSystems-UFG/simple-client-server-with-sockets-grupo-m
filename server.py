29c572a670831c6ae141fb8cace38438357fe564
Thor Franco Brenner <71737933+ThorBrenner@users.noreply.github.com>
2023-05-17 21:39:39 -0300

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:                # forever
   msg = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
   data = pickle.loads(msg)
+  if not data: break 
   if data[0] == 'add':
     result = data[1][0] + data[1][1]
   elif data[0] == 'subtract':
