403f88490e05b4dd29392e8ba0ed57cd8823688b
Murilo <56661791+Murilomsq@users.noreply.github.com>
2023-05-17 21:32:11 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 from socket  import *
 from constCS import * #-
+from pickle
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
+s.send(pickle.dumps('add',(5, 10)))  # send some data
 data = s.recv(1024)     # receive the response
 print (bytes.decode(data))            # print the result
 s.close()               # close the connection
